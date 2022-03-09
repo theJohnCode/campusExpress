3fc0fec5e413e7b7046afc26542bd8d359f17705
JohnCode-s <ifeanyichukwujohn70@gmail.com>
2022-03-09 13:22:46 +0100

    made category model and migration

---
+++ database/migrations/2022_03_09_115435_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('photo')->nullable();
+            $table->boolean('is_parent')->default(true);
+            $table->mediumText('summary')->nullable();
+            $table->unsignedBigInteger('parent_id');
+            $table->foreign('id')->references('category')->onDelete('SET NULL');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
