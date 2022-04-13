cdab2c02d9fc90a6a347c88ebc9b6c2fd19522a4
JohnCode-s <ifeanyichukwujohn70@gmail.com>
2022-04-13 16:31:05 +0100

    started working on the frontend module

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\BannerController;
 use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
@@ -21,15 +22,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* 
+todo|--------------------------------------------------------------
+!           FRONTEND SECTION
+todo|--------------------------------------------------------------
+*/ 
+
+Route::get('/',[IndexController::class,'home']);
+
+
+/* 
+todo|--------------------------------------------------------------
+!           END FRONTEND SECTION
+todo|--------------------------------------------------------------
+*/ 
 
 Auth::routes(['register' => false]);
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-// Admin dashboard
+
+/* 
+todo|--------------------------------------------------------------
+!           ADMIN SECTION
+todo|--------------------------------------------------------------
+*/
+
 Route::group(['prefix' => 'admin', 'middlware' => 'auth'], function(){
     Route::get('/', [AdminController::class, 'admin'])->name('admin');
 
@@ -54,3 +72,9 @@ Route::group(['prefix' => 'admin', 'middlware' => 'auth'], function(){
     Route::resource('user', UserController::class);
     Route::post('/user_status', [UserController::class,'userStatus'])->name('user.status');
 });
+
+/* 
+todo|--------------------------------------------------------------
+!           ADMIN SECTION END
+todo|--------------------------------------------------------------
+*/
